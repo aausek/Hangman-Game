f99dd623330d7d467e82a5c14dc59a99143f005e
Ana Ausek <aausek@Anas-MacBook-Pro.local>
2017-02-18 20:51:10 -0500

    first commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+		
+	//Create array of the alphabet 
+  	var letters = ["A", "B", "C", "D", "E", "F", "G",
+  	"H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+  	"S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+
+	 //Create loop to iterate through the letters array
+	 for (i = 0; i < letters.length; i++) {
+	 	console.log("loop", i);
+	 	 	letterBtn = 
+	 }
+
+
+	});
+ 
